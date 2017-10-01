aea0b32834e05f64813bb50428060996d7a2ba9e
GwonHyeok <me@ghyeok.io>
2017-10-01 22:31:16 +0900

    액셀 저장 기능 구현

--- src/main/crawler.js
+++ src/main/crawler.js
@@ -1,6 +1,8 @@
 const Builder = require('cl-naver-crawler/crawler/builder')
 const BusinessCrawler = require('cl-naver-crawler/crawler/business')
 const StoreFarmCrawler = require('cl-naver-crawler/crawler/storefarm')
+const json2csv = require('json2csv')
+const fs = require('fs')
 
 module.exports = class Crawler {
   getBusinessCrawler (keyword) {
@@ -15,5 +17,18 @@ module.exports = class Crawler {
       .setCrawler(StoreFarmCrawler)
       .setKeyword(keyword)
       .build()
+  }
+
+  saveToCsv (items) {
+    return new Promise((resolve, reject) => {
+      const csv = json2csv({data: items})
+      fs.writeFile('/Users/ghyeok/Desktop/file.csv', csv, function (err) {
+        if (err) {
+          reject(err)
+          throw err
+        }
+        resolve('file saved')
+      })
+    })
   }
 }
--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -35,7 +35,7 @@
                     </div>
                     <div class="column">
                         <div class="is-pulled-right">
-                            <a class="button">
+                            <a class="button" @click="save">
                                 <span>액셀로 저장하기</span>
                             </a>
                         </div>
@@ -65,35 +65,6 @@
             </div>
         </div>
     </section>
-
-    <!--<img id="logo" src="~@/assets/logo.png" alt="electron-vue">-->
-    <!--<main>-->
-    <!--<div class="left-side">-->
-    <!--<span class="title">-->
-    <!--Welcome to your new project!-->
-    <!--</span>-->
-    <!--<system-information></system-information>-->
-    <!--</div>-->
-
-    <!--<div class="right-side">-->
-    <!--<div class="doc">-->
-    <!--<div class="title">Getting Started</div>-->
-    <!--<p>-->
-    <!--electron-vue comes packed with detailed documentation that covers everything from-->
-    <!--internal configurations, using the project structure, building your application,-->
-    <!--and so much more.-->
-    <!--</p>-->
-    <!--<button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs-->
-    <!--</button>-->
-    <!--<br><br>-->
-    <!--</div>-->
-    <!--<div class="doc">-->
-    <!--<div class="title alt">Other Documentation</div>-->
-    <!--<button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>-->
-    <!--<button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>-->
-    <!--</div>-->
-    <!--</div>-->
-    <!--</main>-->
 </template>
 
 <script>
@@ -130,6 +101,12 @@
           this.isLoading = false
         })
         crawler.run()
+      },
+      save () {
+        console.log('save to csv')
+        this.$crawler.saveToCsv(this.items)
+          .then(result => console.log(result))
+          .catch(error => console.error(error))
       }
     }
   }
